457357df386c49ebeb4a03c179109b5455996424
agentnova <arjunaj2234@gmail.com>
2020-09-29 17:36:39 +0530

    Create creds.py

---
+++ creds.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+
+class cred():
+    BOT_TOKEN = os.getenv("BOT_TOKEN") #From botfather
+    API_ID = os.getenv("API_ID")       #"Get this value from my.telegram.org! Please do not steal"
+    API_HASH = os.getenv("API_HASH")   #"Get this value from my.telegram.org! Please do not steal"
+    DB_URL = os.getenv("DB_URL")       #From Firebase database
+
+    ####From Truecaller and Eyecon app request headers respectively########
+
+    T_AUTH = os.getenv("T_AUTH")      # Truecaller auth id CA
+    E_AUTH = os.getenv("F_AUTH")      # Eyecon auth id
+    E_AUTH_V=os.getenv("E_AUTH_V")    # Eyecon auth_v
+    E_AUTH_C=os.getenv("E_AUTH_C")    # Eyecon auth_c
