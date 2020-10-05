f03e5db86a00e7fdf6a559800495d40e4fa5661c
ks406989 <60415968+ks406989@users.noreply.github.com>
2020-10-05 13:26:08 +0530

    Update creds.py

--- creds.py
+++ creds.py
@@ -1,18 +1,25 @@
 import os
+
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-
 class cred():
-    BOT_TOKEN = os.getenv("BOT_TOKEN") #From botfather
-    API_ID = os.getenv("API_ID")       #"Get this value from my.telegram.org! Please do not steal"
-    API_HASH = os.getenv("API_HASH")   #"Get this value from my.telegram.org! Please do not steal"
-    DB_URL = os.getenv("DB_URL")       #From Firebase database
+
+    BOT_TOKEN = os.getenv("1337673070:AAGu_qNlGDxgNaY-3QIodqHh9snKLSkhp30") #From botfather
+
+    API_ID = os.getenv("1144902")       #"Get this value from my.telegram.org! Please do not steal"
+
+    API_HASH = os.getenv("e743e5a4f35076e4c558a4bd713082e9")   #"Get this value from my.telegram.org! Please do not steal"
+
+    DB_URL = os.getenv("https://truecalerbot.firebaseio.com/")       #From Firebase database
 
     ####From Truecaller and Eyecon app request headers respectively########
 
-    T_AUTH = os.getenv("T_AUTH")      # Truecaller auth id CA
-    E_AUTH = os.getenv("E_AUTH")      # Eyecon auth id
-    E_AUTH_V=os.getenv("E_AUTH_V")    # Eyecon auth_v
-    E_AUTH_C=os.getenv("E_AUTH_C")    # Eyecon auth_c
+    T_AUTH = os.getenv("Bearer a1i0u--NIp2_MFJVY8O7bRbZZOQjxTNCtLkWddGx67BwRP5UqX_L41Vc05bNqf1d")      # Truecaller auth id CA
+
+    E_AUTH = os.getenv("93a4faee-9a6d-4908-90e9-8ebb85deddbe")      # Eyecon auth id
+
+    E_AUTH_V=os.getenv("e1")    # Eyecon auth_v
+
+    E_AUTH_C=os.getenv("39")    # Eyecon auth_c
